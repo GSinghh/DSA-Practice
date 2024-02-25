0d77c6c2ea26eb1a339a5687713111fcb06c49d5
GSinghh <gbassans@yahoo.com>
2024-02-25 11:52:31 -0800

    added comments about pointers

--- searching-algos/binary_search.py
+++ searching-algos/binary_search.py
@@ -13,3 +13,7 @@ def binarySearch(target, vals) -> int:
     return - 1
 
 print(binarySearch(4, [1,2,3,4,5,6,7,8,9,10]))
+
+# The left pointer points to the last index where the target value is less than or equal to the element in the array.
+
+# The right pointer points to the first index where the target value is greater than the element in the array.
