97f66bf60363f502a3513755902ecfdd0fff1abf
GSinghh <gbassans@yahoo.com>
2024-03-08 20:35:26 -0800

    Implemented is_empty, insert, and search

--- data-structures/binary_tree.py
+++ data-structures/binary_tree.py
@@ -1,9 +1,54 @@
 class Node:
-    def __init__(self, val) -> None:
-        self.val = val
+    def __init__(self, value) -> None:
+        self.value = value
         self.left = None
         self.right = None
         
 class BinaryTree:
     def __init__(self) -> None:
         self.root = None
+        
+    # Returns True if the BST is empty, False otherwise
+    def is_empty(self) -> bool: 
+        return self.root == None
+        
+    # Insert a new node with the given value into the BST.
+    def insert(self, value) -> None: 
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(value, self.root)
+        
+    # Insert helper function
+    def _insert(self, value, node) -> None:
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert(value, node.left)
+        elif value > node.value:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert(value, node.right)
+        else:
+            print("Value is already in the tree")
+            
+    #Searches for a node with a given value in the BST
+    def search(self, value) -> bool:
+        if self.root:
+            return self._search(value, self.root)
+        else:
+            return False
+        
+    # Search helper function
+    def _search(self, value, node) -> bool:
+        if node.value == value:
+            return True
+        elif value < node.value:
+            return self._search(value, node.left)
+        elif value > node.value:
+            return self._search(value, node.right)
+        else:
+            return False
+        
