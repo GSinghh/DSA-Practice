52da233857367c6eb3f6a39db1c09b10015eb426
GSinghh <gbassans@yahoo.com>
2024-03-12 23:45:52 -0700

    Implementation of Set

---
+++ data-structures/set.py
@@ -0,0 +1,20 @@
+class Set:
+    def __init__(self) -> None:
+        self.container = []
+        
+    def add(self, val):
+        if val not in self.container:
+            self.container.append()
+    
+    def remove(self, val):
+        if val in self.container:
+            self.container.remove(val)
+            
+    def contains(self, val):
+        return val in self.container
+    
+    def length(self):
+        return len(self.container)
+    
+    def display(self):
+        print(self.container, end=" ")
