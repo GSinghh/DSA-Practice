f3379940d61093983fce7892cd37cd4d9f34e4a2
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-04-24 23:23:47 -0700

    Fixed implementation

--- techniques/sliding_window_fixed.py
+++ techniques/sliding_window_fixed.py
@@ -11,11 +11,13 @@ def slidingWindow(nums, k):
 # slidingWindow([1,2,3,4,5,6,7,8,9,10], 3)
 
 def sliding_window_sum(nums, k):
+    res = 0
     window_sum = sum(nums[:k])
-    print(f"Initial Window {nums[:k]} Initial Window Sum {window_sum}")
+    res = max(res, window_sum)
     for i in range(k, len(nums)):
-        window_sum = window_sum + nums[i] - nums[i - k]
-        window = nums[i - k + 1 :i + 1]
-        print(f"Window: {window} \nWindow_Sum: {window_sum}")
+        window_sum += nums[i]
+        window_sum -= nums[i - k]
+        res = max(res, window_sum)
+    return res
         
 sliding_window_sum([1,4,5,67,4,1,7,9,1], 3)
