70b2049ce785bdc2c46c51c75cead816b9c593b6
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-05-06 14:59:26 -0700

    Fixed implementation

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,9 +1,8 @@
 def fibonacci(n):
-    if n <= 0:
-        return "Invalid number"
-    if n == 1 or n == 2:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
     
-print(fibonacci(9))
+    return fibonacci(n - 1) + fibonacci(n - 2)
+    
